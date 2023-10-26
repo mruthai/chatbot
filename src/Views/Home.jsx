486dc93f29da18d28ccaac51a9d2240840690247
Martin Ruthaivilavan <mmvilavan@gmail.com>
2023-10-26 16:08:52 -0500

    first commit

---
+++ src/Views/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="bg-slate-600 max-h-auto">
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="text-white text-3xl">chatbot</h1>
+            </div>
+    </div>
+  )
+}
+
+export default Home
